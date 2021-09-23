b833c313c803b6eaefd645af9ab594c0e58c56cb
Kevin Faveri <kevin@faveri.dev>
2021-09-23 11:42:53 -0300

    fix: tailwind prod build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media',
   theme: {
     extend: {},
